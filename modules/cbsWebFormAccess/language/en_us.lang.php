dc290c46043703b1d99a94b1dcda4194884eff38
= <=>
2014-11-27 16:38:15 +0100

    Webform Access

---
+++ modules/cbsWebFormAccess/language/en_us.lang.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+    'cbsWebFormAccess' => 'Webform Access',
+    'cbsWebFormAccess' => 'Webform Access',
+    'webformaccessno' => 'Webform Access No.',
+    'access_date' => 'Access Date',
+    'access_time' => 'Access Time',
+    'relid' => 'Related to',
+    'access_url' => 'Access URL',
+    'access_web' => 'Access Web',
+    'submit' => 'Submit',
+
+    'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+    'LBL_CBSWEBFORMACCESS_INFORMATION' => 'Webform Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Description',
+);
+
+?>
---
+++ modules/cbsWebFormAccess/language/it_it.lang.php
@@ -0,0 +1,20 @@
+<?php
+$mod_strings = array (
+//label of the distributor module
+    'cbsWebFormAccess' => 'Webform Access',
+    'cbsWebFormAccess' => 'Webform Access',
+    'webformaccessno' => 'Webform Access No.',
+    'access_date' => 'Accesso Data',
+    'access_time' => 'Accesso Tempo',
+    'relid' => 'Relato a',
+    'access_url' => 'Accesso URL',
+    'access_web' => 'Accesso Web',
+    'submit' => 'Submit',
+
+    'LBL_CUSTOM_INFORMATION' => 'Informazioni Personalizzate',
+    'LBL_CBSWEBFORMACCESS_INFORMATION' => 'Webform Access Information',
+    'LBL_DESCRIPTION_INFORMATION' => 'Informazioni Descrizione',
+ //end
+);
+
+?>
